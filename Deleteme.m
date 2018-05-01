5b65965fb03d49951a0383177e98bec8a692d6b9
Mohamed Abdelpakey <mha241@mun.ca>
2018-05-01 16:25:55 -0230

    Add files via upload

---
+++ Deleteme.m
@@ -0,0 +1,6 @@
+per_frame_annotation('F:\ENTIREIMGNET\ILSVRC2015\', 4)
+
+parse_objects('F:\ENTIREIMGNET\ILSVRC2015\', 1)
+parse_objects('F:\ENTIREIMGNET\ILSVRC2015\', 2)
+   parse_objects('F:\ENTIREIMGNET\ILSVRC2015\', 3)
+    parse_objects('F:\ENTIREIMGNET\ILSVRC2015\', 4)
